2804aa7520602326b33c84d2db5f5e349fef11f5
stardvst <lilia.manukyan@hotmail.com>
2024-03-30 21:03:26 +0400

    added a new type to acyclic visitor

--- ch4-visitor-design-pattern/5-acyclic-visitor/draw.h
+++ ch4-visitor-design-pattern/5-acyclic-visitor/draw.h
@@ -5,11 +5,13 @@
 #include "visitor.h"
 #include "circle.h"
 #include "square.h"
+#include "triangle.h"
 
 class Draw
   : public AbstractVisitor
   , public Visitor<Circle>
   , public Visitor<Square>
+  , public Visitor<Triangle>
 {
 public:
   void visit(const Circle &circle) const override
@@ -20,5 +22,10 @@ public:
   void visit(const Square &square) const override
   {
     std::cout << "drawing square... side=" << square.side() << ", center x/y=" << square.center().x << '/' << square.center().y << '\n';
+  }
+
+  void visit(const Triangle &triangle) const
+  {
+    std::cout << "drawing triangle... p1=(" << triangle.p1().x << ", " << triangle.p1().y << "), p2=(" << triangle.p2().x << ", " << triangle.p2().y << "), p3=(" << triangle.p3().x << ", " << triangle.p3().y << ")\n";
   }
 };
---
+++ ch4-visitor-design-pattern/5-acyclic-visitor/triangle.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <utility>
+#include "point.h"
+#include "shape.h"
+#include "abstractVisitor.h"
+#include "visitor.h"
+
+class Triangle : public Shape
+{
+public:
+  explicit Triangle(Point p1, Point p2, Point p3)
+      : m_p1(std::move(p1)), m_p2(std::move(p2)), m_p3(std::move(p3))
+  {
+  }
+
+  Point p1() const { return m_p1; }
+  Point p2() const { return m_p2; }
+  Point p3() const { return m_p3; }
+
+  void accept(const AbstractVisitor &visitor) const override
+  {
+    if (const auto *traingleVisitor = dynamic_cast<const Visitor<Triangle> *>(&visitor))
+      traingleVisitor->visit(*this);
+  }
+
+private:
+  Point m_p1{}, m_p2{}, m_p3{};
+};
